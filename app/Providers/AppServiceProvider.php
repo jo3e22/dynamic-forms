e742d2f9c0697deed896bf727436648465619559
jo3e22 <jo3e22@soton.ac.uk>
2025-12-15 23:01:21 +0000

    feat: Force HTTPS scheme in production environment

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\URL;
 use App\Models\Form;
 use App\Policies\FormPolicy;
 use Inertia\Inertia;
@@ -24,6 +25,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
+
         Gate::policy(Form::class, FormPolicy::class);
 
         Inertia::share([
